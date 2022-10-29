bb8c5f0fcabd3154b1a0f3bb4b96b119aee866cb
AlvaroPV <100451185@alumnos.uc3m.es>
2022-10-29 18:07:57 +0200

    added requirements.txt

--- src/main.py
+++ src/main.py
@@ -1,14 +1,9 @@
 from ui.Interface import Interface
-from ui.InterfaceTuto import InterfaceTuto
-
 # Simple library encrypted exercise.
 
 def main():
     interface = Interface()
     interface.mainloop()
-
-    interfaceTuto = InterfaceTuto()
-    #interfaceTuto.mainloop()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
