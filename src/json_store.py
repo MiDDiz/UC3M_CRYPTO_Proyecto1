784251670f2da83180c3290a0f062ae2d3e101d0
AlvaroPV <100451185@alumnos.uc3m.es>
2022-10-29 18:02:05 +0200

    generated new class Json with methosd to load/store information

--- src/json_store.py
+++ src/json_store.py
@@ -7,17 +7,16 @@ class JsonStore:
 
     """The method save_data is used to store a given data in a json file.
     It overwrites the new data on the file"""
-    def save_data(self, data:list[dict], type:str):
-        path= self.file_path+type+".json"
-        with open(path, "w", encoding="utf-8",newline="") as file:
+    def save_data(self, data:list[dict]):
+        with open(self.file_path, "w", encoding="utf-8",newline="") as file:
             json.dump(data, file, indent=2)
 
     """This method returns a list with all the data that is found on a 
     json file"""
-    def load_data(self,type:str)->list[dict]:
-        path = self.file_path + type + ".json"
+    def load_data(self)->list[dict]:
+
         try:
-            with open(path, "r", encoding="utf-8", newline="") as file:
+            with open(self.file_path, "r", encoding="utf-8", newline="") as file:
                 newdata=json.load(file)
         except:
             newdata=[]
@@ -25,30 +24,30 @@ class JsonStore:
 
     """The method finds if there is some data connected to the same user
     as the new data"""
-    def find_item(self, new_item, type):
-        data = self.load_data(type)
+    def find_item(self, new_item):
+        data = self.load_data()
         for item in data:
             if item["usuario"] == new_item["usuario"]:
                 return item
         return None
 
     """addnew is only used to store new items on the data"""
-    def addnew(self, item:dict, type:str):
-        data = self.load_data(type)
+    def addnew(self, item:dict):
+        data = self.load_data()
         data.append(item)
-        self.save_data(data, type)
+        self.save_data(data)
 
     """replace_items replaces the item given with the newitem on the json """
-    def replace_item(self, item, newitem, type):
-        data = self.load_data(type)
+    def replace_item(self, item, newitem):
+        data = self.load_data()
         data.remove(item)
         data.append(newitem)
-        self.save_data(data, type)
+        self.save_data(data)
 
     """additem is used to add new items, and replace them in case they already exist"""
-    def additem(self, item:dict, type:str):
-        found_item=self.find_item(item,type)
+    def additem(self, item:dict):
+        found_item=self.find_item(item)
         if found_item==None:
-            self.addnew(item,type)
+            self.addnew(item)
         else:
-            self.replace_item(found_item,item,type)
+            self.replace_item(found_item,item)
